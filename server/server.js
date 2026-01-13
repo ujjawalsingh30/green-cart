0e216d352f56f37ebfe2c4385b793118f2e2a142
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-01-14 03:14:42 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -1,33 +1,77 @@
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import cors from 'cors';
-import connectDB from './configs/db.js';
-import 'dotenv/config';
-import userRouter from './routes/UserRoute.js';
-import sellerRouter from './routes/sellerRoute.js';
-import connectCloudinary from './configs/cloudinary.js';
-import productRouter from './routes/productRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import addressRouter from './routes/addressRoute.js';
-import orderRouter from './routes/orderRoute.js';
-import { stripeWebhooks } from './controllers/orderController.js';
+// import cookieParser from 'cookie-parser';
+// import express from 'express';
+// import cors from 'cors';
+// import connectDB from './configs/db.js';
+// import 'dotenv/config';
+// import userRouter from './routes/UserRoute.js';
+// import sellerRouter from './routes/sellerRoute.js';
+// import connectCloudinary from './configs/cloudinary.js';
+// import productRouter from './routes/productRoute.js';
+// import cartRouter from './routes/cartRoute.js';
+// import addressRouter from './routes/addressRoute.js';
+// import orderRouter from './routes/orderRoute.js';
+// import { stripeWebhooks } from './controllers/orderController.js';
 
+
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// await connectDB()
+// await connectCloudinary()
+
+// // Allow multiple origins
+// const allowedOrigins = ['http://localhost:5173']
+
+// app.post('/strie', express.raw({type: 'application/json'}), stripeWebhooks)
+
+// app.post('/stripe', express.raw({type: 'application.json'}), stripeWebhooks)
+
+
+// // Middlware Configuration
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({origin: allowedOrigins, credentials: true}));
+
+// app.get('/', (req, res) => res.send("API is Working"));
+// app.use('/api/user', userRouter)
+// app.use('/api/seller', sellerRouter)
+// app.use('/api/product', productRouter)
+// app.use('/api/cart', cartRouter)
+// app.use('/api/address', addressRouter)
+// app.use('/api/order', orderRouter)
+
+
+// app.listen(port, ()=>{
+//    console.log(`Server is running on http://localhost:${port}`)
+// })
+
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import 'dotenv/config'
+import userRouter from "./routes/userRoute.js";
+import sellerRouter from "./routes/sellerRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import { stripeWebhooks } from "./controllers/orderController.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Connecting Database & Cloudinary
 await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-zeta-coral.vercel.app', 'https://greencart-frontend-g4az.onrender.com']
 
-app.post('/strie', express.raw({type: 'application/json'}), stripeWebhooks)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
-app.post('/stripe', express.raw({type: 'application.json'}), stripeWebhooks)
-
-
-// Middlware Configuration
+// Middleware Configuration
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
@@ -40,7 +84,7 @@ app.use('/api/cart', cartRouter)
 app.use('/api/address', addressRouter)
 app.use('/api/order', orderRouter)
 
-
-app.listen(port, ()=>{
-   console.log(`Server is running on http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    
 })
